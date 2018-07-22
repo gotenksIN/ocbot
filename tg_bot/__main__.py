913a44c4ebf92ebc3901e5cde3741f432d6bcb93
songotenks69 <forumomkar@gmail.com>
2018-07-22 14:55:14 +0530

    Update the donation message

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -54,7 +54,11 @@ DONATE_STRING = """Heya, glad to hear you want to donate!
 It took lots of work for [Paul](t.me/SonOfLars) to get me to where I am now, and every donation helps \
 motivate him to make me even better. All the donation money will go to a better VPS to host Marie , and/or beer \
 (see his bio!). He's just a poor student, so every little helps!
-There are two ways of paying him; [PayPal](paypal.me/PaulSonOfLars), or [Monzo](monzo.me/paulnionvestergaardlarsen)."""
+There are two ways of paying him; [PayPal](paypal.me/PaulSonOfLars), or [Monzo](monzo.me/paulnionvestergaardlarsen).
+
+For all the memes , math and dogbin, thanks Till Kottman aka deletescape . Donate him via [PayPal](paypal.me/deletescape)
+
+For all the extra sudo commands and birthday , thanks Rhyse Changuion aka Skittles9823. Donate him via [PayPal](https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=ZQ7RRADXTPVBA)"""
 
 IMPORTED = {}
 MIGRATEABLE = []
